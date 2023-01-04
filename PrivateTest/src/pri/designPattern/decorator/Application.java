e88b34b85ba3870c9465ac87b8302e0fc59c7b4a
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2023-01-04 09:46:11 +0900

    연습문제 구체적인 데코레이터 추가

--- PrivateTest/src/pri/designPattern/decorator/Application.java
+++ PrivateTest/src/pri/designPattern/decorator/Application.java
@@ -4,23 +4,23 @@ public class Application {
 	
 	public static void main(String[] args) {
 		Display b1 = new StringDisplay("Hello, World.");
-		Display b2 = new SideBorder(b1, '#');
-		Display b3 = new FullBorder(b2);
+		Display b2 = new UpDownBorder(b1, '-');
+		Display b3 = new SideBorder(b2, '*');
 		
 		b1.show();
 		b2.show();
 		b3.show();
 		
-		Display b4 = new SideBorder(
-						new FullBorder(
-							new FullBorder(
-								new SideBorder(
-									new FullBorder(
+		Display b4 = new FullBorder(
+						new UpDownBorder(
+							new SideBorder(
+								new UpDownBorder(
+									new SideBorder(
 										new StringDisplay("Hello.")
-									), '*'
-								)
-							)
-						), '/'
+									, '*')
+								,'=')
+							, '|')
+						, '/')
 					);
 		
 		b4.show();
