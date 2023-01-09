26f6b808615e5d9eff31ba2f09b71176fa8a2b16
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2023-01-09 18:32:50 +0900

    Observer 패턴 NumberGenerator(Subject) 구현체 추가

--- PrivateTest/src/pri/designPattern/observer/Application.java
+++ PrivateTest/src/pri/designPattern/observer/Application.java
@@ -3,7 +3,7 @@ package pri.designPattern.observer;
 public class Application {
 
 	public static void main(String[] args) {
-		NumberGenerator generator = new RandomNumberGenerator();
+		NumberGenerator generator = new IncrmentalNumberGenerator(10, 50, 5);
 		Observer observer1 = new DigitObserver();
 		Observer observer2 = new GraphObserver();
 		
