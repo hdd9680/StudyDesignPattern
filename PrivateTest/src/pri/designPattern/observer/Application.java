f0daf02820951d5ffb986b1f1432aa1757fb9163
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2023-01-09 18:31:24 +0900

    Observer 패턴 예제 완료

---
+++ PrivateTest/src/pri/designPattern/observer/Application.java
@@ -0,0 +1,15 @@
+package pri.designPattern.observer;
+
+public class Application {
+
+	public static void main(String[] args) {
+		NumberGenerator generator = new RandomNumberGenerator();
+		Observer observer1 = new DigitObserver();
+		Observer observer2 = new GraphObserver();
+		
+		generator.addObserver(observer1);
+		generator.addObserver(observer2);
+		generator.execute();
+	}
+
+}
